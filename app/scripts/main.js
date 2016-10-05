b74731c0b84059ad75e4096024d7b8ddf1f87502
takeshi <kikuchi@gmail.com>
2016-10-05 16:10:01 +0900

    update csv

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,51 +1,42 @@
-Papa.parse("data/mapdata.csv", {
-	complete: function(results) {
-		console.log(results);
-	}
-});
+var csvjson={};
+(function(){function k(e){return"undefined"==typeof e?!1:!0}function m(e,d){for(var c=e.split(d=d||","),a=c.length-1,b;0<=a;a--)'"'==c[a].replace(/"\s+$/,'"').charAt(c[a].length-1)?1<(b=c[a].replace(/^\s+"/,'"')).length&&'"'==b.charAt(0)?c[a]=c[a].replace(/^\s*"|"\s*$/g,"").replace(/""/g,'"'):a?c.splice(a-1,2,[c[a-1],c[a]].join(d)):c=c.shift().split(d).concat(c):c[a].replace(/""/g,'"');return c}csvjson.csv2json=function(e,d){var d=d||{},c=k(d.delim)?d.delim:",",a=e.split("\n"),b=m(a[0],c),a=a.slice(1,
+a.length),f={};f.headers=b;f.rows=[];for(var h in a){var g=a[h],i=m(g,c);if(0==g.length)break;var g={},l;for(l in i){var j=i[l];isNaN(1*j)||(j*=1);g[b[l]]=j}f.rows.push(g)}return f};csvjson.json2csv=function(e,d){var d=d||{},c=k(d.delim)?d.delim:",",a=k(d.textdelim)?d.textdelim:"";if("string"==typeof e)return null;var b="",f;for(f in e.headers)var h=e.headers[f],b=b+(a+h+a+c);var b=b.slice(0,b.length-1),b=b+"\n",g;for(g in e.rows){var i=e.rows[g];for(f in e.headers)h=e.headers[f],b="string"==typeof i[h]?
+b+(a+i[h]+a+c):b+(i[h]+c);b=b.slice(0,b.length-1);b+="\n"}return b=b.slice(0,b.length-1)}})();
 
-var map = new GMaps({
-  el: '#map',
-  lat: 35.6931623,
-  lng: 138.587116,
-  zoom: 7
-});
+$(function(){
+  $.ajax({
+    url: "data/mapdata.csv",
+    success: function(data){
 
-map.addMarkers([
-  {
-    lat: 35.600413,
-    lng: 140.097878,
-    title: 'japan',
-    infoWindow: {
-      content: '<a href="https://retrip.jp/articles/66226/" target="_blank"><figure><img src="https://static.retrip.jp/article/66226/images/6622621280c62-5267-4878-b945-261b7615b17c_s.jpg" width="120"></figure><p>360度美しい！夜景遺産に認定された"千葉ポートタワー"が絶景すぎる</p></a>'
+      var map = new GMaps({
+        el: '#map',
+        lat: 35.6931623,
+        lng: 138.587116,
+        zoom: 1
+      });
+
+      var _result = csvjson.csv2json(data, {
+    		delim: ","
+    	});
+      var _array = [];
+      for(var i = 0, ileng = _result.rows.length; i < ileng; i++){
+        var _temp = _result.rows[i];
+        var _content = "";
+        _content += '<a href="' + _temp.url + '" target="_blank"><figure><img src="' + _temp.thumbnail + '" width="120">';
+        if(_temp.credit) {
+          _content += "<figcaption>" + _temp.credit + "</figcaption>";
+        }
+        _content += '</figure><p>' + _temp.title + '</p></a>';
+        _array.push({
+          "lat": Number(_temp["latitude"]),
+          "lng": Number(_temp["longitude"]),
+          "title": "japan",
+          infoWindow: {
+            "content": _content
+          }
+        })
+      }
+      map.addMarkers(_array);
     }
-  },{
-    lat: 35.044128,
-    lng: 135.787383,
-    title: 'japan',
-    infoWindow: {
-      content: '<a href="https://retrip.jp/articles/58167/" target="_blank"><figure><img src="https://static.retrip.jp/article/58167/images/5816759ba4140-d03e-46e2-8dd1-b16380af6c3b_s.jpg" width="120"></figure><p>年間800杯食べるマニアが厳選！京都・一乗寺周辺の激ウマオススメラーメンBEST12</p></a>'
-    }
-  },{
-    lat: 35.336588,
-    lng: 139.644753,
-    title: 'japan',
-    infoWindow: {
-      content: '<a href="https://retrip.jp/articles/66145/" target="_blank"><figure><img src="https://static.retrip.jp/article/66145/images/661451b88e7ee-0c75-4637-8f4c-4c9b1a6f03c5_s.jpg" width="120"></figure><p>【明日から】秋花火も楽しめる！「八景島シーパラダイス」で初のハロウィンイベント開催</p></a>'
-    }
-  },{
-    lat: 35.322614,
-    lng: 139.552717,
-    title: 'japan',
-    infoWindow: {
-      content: '<a href="https://retrip.jp/articles/66174/" target="_blank"><figure><img src="https://static.retrip.jp/article/66174/images/66174c8cf22e1-1572-46c0-b636-c2d5d6fe014e_s.jpg" width="120"></figure><p>ディズニーラテアートが可愛すぎる！鎌倉にあるカフェ「雫」がSNSで話題</p></a>'
-    }
-  },{
-    lat: 35.010219,
-    lng: 135.677001,
-    title: 'japan',
-    infoWindow: {
-      content: '<a href="https://retrip.jp/articles/66164/" target="_blank"><figure><img src="https://static.retrip.jp/article/66164/images/66164957b6297-f8bb-4e17-942e-0275fe652156_s.jpg" width="120"></figure><p>秋の星空観賞！京都・嵐山で星空の楽しみ方を集めた「宙フェス2016」開催</p></a>'
-    }
-  }
-]);
+  })
+});
